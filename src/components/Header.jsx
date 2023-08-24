d401bf4dd2b7294466347dc9d233acd7c4cd0f08
shawranjit040 <126514491+shawranjit040@users.noreply.github.com>
2023-08-24 19:47:26 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,6 +81,7 @@ const Header = () => {
                     />
                 </div>
                 <button
+                    style={{color: "white"}}
                     className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]"
                     onClick={() => searchQueryHandler("searchButton")}
                 >
